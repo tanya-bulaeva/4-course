161e65c713d2cd581303e7f188d4488bd1fb0b21
Tanya Bulaeva <t.akberdina@gmail.com>
2023-09-09 11:27:15 +0500

    3 компонента

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import NavMenu from './components/navMenu.jsx'
+import Search from './components/search.jsx';
+import Filter from './components/filter.jsx';
 function App() {
   return (
     <div className="wrapper">
@@ -7,28 +9,9 @@ function App() {
         <main className="main">
         <NavMenu />
           <div className="main__centerblock centerblock">
-            <div className="centerblock__search search">
-              <svg className="search__svg">
-                <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-              </svg>
-              <input
-                className="search__text"
-                type="search"
-                placeholder="Поиск"
-                name="search"
-              />
-            </div>
+        <Search />
             <h2 className="centerblock__h2">Треки</h2>
-            <div className="centerblock__filter filter">
-              <div className="filter__title">Искать по:</div>
-              <div className="filter__button button-author _btn-text">
-                исполнителю
-              </div>
-              <div className="filter__button button-year _btn-text">
-                году выпуска
-              </div>
-              <div className="filter__button button-genre _btn-text">жанру</div>
-            </div>
+          <Filter />
             <div className="centerblock__content">
               <div className="content__title playlist-title">
                 <div className="playlist-title__col col01">Трек</div>
