1248591539c037237430539f9ed6fd6ef4465f8c
Tanya Bulaeva <t.akberdina@gmail.com>
2023-09-17 10:12:08 +0500

    настроены фильтры

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -40,15 +40,15 @@ export default function Filter(){
       <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
       <div className="dropdown">
-       { openArtist ? (<><div className = "filter__button button-year _btn-text" onClick = {toggleVisibilityArtist}>исполнителю</div>
-        <FilterByArtist/></>): ( <><div className = "filter__button button-year _btn-text" onClick = {toggleVisibilityArtist}>исполнителю</div>
+       { openArtist ? (<><button className = "button-active _btn-text" onClick = {toggleVisibilityArtist}>исполнителю</button>
+        <FilterByArtist/></>): ( <><div className = "filter__button  _btn-text" onClick = {toggleVisibilityArtist}>исполнителю</div>
 </>)
      } 
-     {openYear ? (<> <div className="filter__button button-year _btn-text" onClick = {toggleVisibilityYear}>году выпуска</div>
-       <FilterByYear/></>) : (<><div className="filter__button button-year _btn-text" onClick = {toggleVisibilityYear}>году выпуска</div></>)}
+     {openYear ? (<> <button className="button-active _btn-text" onClick = {toggleVisibilityYear}>году выпуска</button>
+       <FilterByYear/></>) : (<><div className="filter__button  _btn-text" onClick = {toggleVisibilityYear}>году выпуска</div></>)}
 
-     {openGenre ? (<><div className="filter__button button-genre _btn-text" onClick = {toggleVisibilityGenre}>жанру</div>
-        <FilterByGenre/></>) : (<><div className="filter__button button-genre _btn-text" onClick = {toggleVisibilityGenre}>жанру</div></>)}
+     {openGenre ? (<><button className="button-active _btn-text" onClick = {toggleVisibilityGenre}>жанру</button>
+        <FilterByGenre/></>) : (<><div className="filter__button  _btn-text" onClick = {toggleVisibilityGenre}>жанру</div></>)}
      </div>
      
      </div>
