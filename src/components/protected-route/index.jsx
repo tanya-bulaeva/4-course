ae3fc4de32bef3e95941258f8ef57ebcec975464
Tanya Bulaeva <t.akberdina@gmail.com>
2023-11-12 16:39:08 +0500

    исправлено

--- src/components/protected-route/index.jsx
+++ src/components/protected-route/index.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import MainBlock from "../mainBlock/MainBlock";
+import MainBlock from "../MainBlock/MainBlock";
 
 export const ProtectedRoute = ({ redirectPath = "/login"}) => {
   if (!localStorage.getItem('user')) { //getItem(key) – получить данные по ключу key.
